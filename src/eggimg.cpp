808db4af4dc24b67474f2f90cd8175c43f136d1c
eggmoe <akelleyworks@gmail.com>
2026-06-04 04:06:59 -0700

    rendering quad to screen

--- src/eggimg.cpp
+++ src/eggimg.cpp
@@ -5,6 +5,7 @@
 #include <Windows.h>
 #include "eggimg.h"
 #include <iostream>
+#include <cassert>
 
 
 
@@ -14,10 +15,117 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 HDC windowHandleToDeviceContext;
 
+//vertex shader
+static const char* vertexShaderSource = R"(
+#version 330 core
+
+layout(location = 0) in vec2 aPosition;
+layout(location = 1) in vec2 aTexCoord;
+
+out vec2 vTexCoord;
+
+void main()
+{
+    vTexCoord = aTexCoord;
+    gl_Position = vec4(aPosition, 0.0, 1.0);
+}
+)";
+// fragment shader
+static const char* fragmentShaderSource = R"(
+#version 330 core
+
+in vec2 vTexCoord;
+
+uniform sampler2D uTexture;
+
+out vec4 fragColor;
+
+void main()
+{
+    //fragColor = texture(uTexture, vTexCoord);
+	fragColor = vec4(vTexCoord.x,vTexCoord.y,1.0,1.0);
+}
+)";
+
+float verts[] =
+{
+	-1.f,1.f,
+	-1.f,-1.f,
+	1.f,-1.f,
+
+	-1.f,1.f,
+	1.f,-1.f,
+	1.f,1.f
+};
+
+float uvs[] =
+{
+	0.f,1.f,
+	0.f,0.f,
+	1.f,0.f,
+
+	0.f,1.f,
+	1.f,0.f,
+	1.f,1.f
+};
+
+GLuint vbo;
+GLuint vao;
+GLuint shaderProgram;
+
+void GenerateBoxMesh(void)
+{
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(verts), verts, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, 0);
+	glEnableVertexAttribArray(0);
+
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(uvs), uvs, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, 0);
+	glEnableVertexAttribArray(1);
+}
+
+void GenerateShader(void)
+{
+	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+	GLint status;
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &status);
+	assert(status == GL_TRUE && "vertex shader failed to compile");
+
+	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &status);
+	assert(status == GL_TRUE && "fragment shader failed to compile");
+
+	shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+
+	glLinkProgram(shaderProgram);
+	glUseProgram(shaderProgram);
+
+}
+
 void Draw(void)
 {
+	glClearColor(0.1f, 0.1f, 0.2f, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+	glUseProgram(shaderProgram);
+	glBindVertexArray(vao);
+
+	glDrawArrays(GL_TRIANGLES, 0, 6);
 	SwapBuffers(windowHandleToDeviceContext);
 }
 
@@ -135,7 +243,8 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			MessageBoxA(0, "Failed to initialize GLAD!", "Error", MB_ICONERROR);
 			return -1;
 		}
-
+		GenerateBoxMesh();
+		GenerateShader();
 		char* string = (char*)glGetString(GL_VERSION);
 		std::cout << "OPENGL VERSION: " << string << std::endl;
 		//MessageBoxA(hwnd, string, "OPENGL VERSION", 0);
@@ -155,5 +264,6 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	}
 	return 0;
 }
+
 
 
